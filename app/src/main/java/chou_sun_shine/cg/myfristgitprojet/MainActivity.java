2caecd0dc57838e832710df2628f30e143148101
HarveyOne <harveyander1@gmail.com>
2019-03-01 19:01:51 +0100

    first commit

---
+++ app/src/main/java/chou_sun_shine/cg/myfristgitprojet/MainActivity.java
@@ -0,0 +1,13 @@
+package chou_sun_shine.cg.myfristgitprojet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
